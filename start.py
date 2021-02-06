f34c8eac6837a53dc2914d0ff8ce70b51874eba4
Home iMac <homeimac@Homes-iMac.local>
2021-02-06 13:41:26 +0545

    first commit

---
+++ start.py
@@ -0,0 +1,19 @@
+import os
+
+
+#edit this part
+hosts = [
+        'wish-dating-sudden-welfare.trycloudflare.com',
+        'albuquerque-hu-ability-disturbed.trycloudflare.com'
+        ]
+
+walletID = '0xC4fD61000DFFf9B16B3621916473A124503762ED'
+email = 'cominlooks@gmail.com'
+password = 's'
+
+
+
+
+hosts = ' '.join(hosts)
+# os.system("python3 bs2.py -ho " + str(hosts) + " -w " + str(walletID) + " -e " + str(email) + " -p " + str(password))
+os.system("./bs2/bs2 -ho " + str(hosts) + " -w " + str(walletID) + " -e " + str(email) + " -p " + str(password))
